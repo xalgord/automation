fd5bcc030de15a0303bf78a3c30a37ac4537b8d3
Krishna Kumar <xalgord@gmail.com>
2024-07-09 13:27:46 +0530

    Update and rename subenum.sh to auto.sh

--- auto.sh
+++ auto.sh
@@ -14,6 +14,13 @@ NC='\033[0m' # No Color
 mkdir -p ~/targets/$domain/recon
 cd ~/targets/$domain/recon
 
+# Remove separate files
+echo -e "${BLUE}-----------------------------------------"
+echo -e "${GREEN}Removing separate files..."
+echo -e "${BLUE}-----------------------------------------${NC}"
+touch "test.txt"
+rm -rf *
+
 # Download and unzip the subdomains file from chaos
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Downloading and unzipping the subdomains file from chaos..."
@@ -34,12 +41,6 @@ echo -e "${GREEN}Getting subdomains from crt.sh..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 curl -s 'https://crt.sh/?q=%.'$domain'&output=json' | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > crt.txt
 
-# Run Subfinder
-echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running Subfinder..."
-echo -e "${BLUE}-----------------------------------------${NC}"
-subfinder -d $domain -silent -all -nW -o subfinder.txt
-
 # Run Subfinder recursively
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running Subfinder recursively..."
@@ -58,11 +59,11 @@ echo -e "${GREEN}Running Spyhunt..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 python3 ~/tools/spyhunt/spyhunt.py -s $domain -sv spyhunt.txt
 
-# Run bbot
-echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running bbot..."
-echo -e "${BLUE}-----------------------------------------${NC}"
-printf '\n%.0s' {1..3} | bbot -t $domain -f subdomain-enum -o .
+# # Run bbot
+# echo -e "${BLUE}-----------------------------------------"
+# echo -e "${GREEN}Running bbot..."
+# echo -e "${BLUE}-----------------------------------------${NC}"
+# printf '\n%.0s' {1..3} | bbot -t $domain -f subdomain-enum -o .
 
 # Merge all subdomains into all.txt
 echo -e "${BLUE}-----------------------------------------"
@@ -70,17 +71,11 @@ echo -e "${GREEN}Merging all subdomains into all.txt..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 cat *.txt */subdomains.txt | sort -u > ../all.txt
 
-# Remove separate files
-echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Removing separate files..."
-echo -e "${BLUE}-----------------------------------------${NC}"
-rm -rf *
-
-# new subdomains
-echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}New subdomains found..."
-echo -e "${BLUE}-----------------------------------------${NC}"
-cat ../all.txt | anew ../new-subdomains.txt | tee ../new.txt
+# # new subdomains
+# echo -e "${BLUE}-----------------------------------------"
+# echo -e "${GREEN}New subdomains found..."
+# echo -e "${BLUE}-----------------------------------------${NC}"
+# cat ../all.txt | anew ../new-subdomains.txt | tee ../new.txt
 
 # Extracting important subdomains
 echo -e "${BLUE}-----------------------------------------"
@@ -89,17 +84,24 @@ echo -e "${BLUE}-----------------------------------------${NC}"
 cd ..
 python3 ~/tools/spyhunt/spyhunt.py -isubs new.txt
 
+# Running HTTPX
+echo -e "${BLUE}-----------------------------------------"
+echo -e "${GREEN}Running httpx on all.txt..."
+echo -e "${BLUE}-----------------------------------------${NC}"
+cat all.txt | httpx -sc -title -cl -probe | grep -v "FAILED" | anew juicy-live.txt
+cat juicy-live.txt | sed 's|^[^/]*//||' | cut -d '/' -f 1 | cut -d " " -f 1 > new.txt 
+
 # Port Scan
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running Port Scan..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-smap -iL juice_subs.txt > ports.txt
+naabu -l new.txt -o ports.txt
 
-# Running HTTPX
+# Running Nuclei
 echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running httpx..."
+echo -e "${GREEN}Running nuclei scan..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-cat juice_subs.txt | httpx -sc -title -cl -probe | grep -v "FAILED" | anew juicy-live.txt
+nuclei -l new.txt -es info,low | tee vulns.txt
 
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${YELLOW}Reconnaissance completed for $domain"
