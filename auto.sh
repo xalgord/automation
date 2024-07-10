67c598625bcd3cf2f5876611596be13d80f72800
Krishna Kumar <xalgord@gmail.com>
2024-07-10 14:33:11 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -76,7 +76,7 @@ clear
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Getting endpoints..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-cat new.txt | gau > anew endpoints.txt
+cat new.txt | gau > endpoints.txt
 waymore -i lyft.com -mode U -oU waymore.txt && cat waymore.txt | anew endpoints.txt
 clear
 
