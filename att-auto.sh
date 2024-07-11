90740191a576106d41f94c03c1a801df93004d7b
Krishna Kaushal <xalgord@gmail.com>
2024-07-11 15:03:15 +0530

    updated

--- att-auto.sh
+++ att-auto.sh
@@ -18,7 +18,6 @@ cd ~/targets/$domain/recon
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Removing separate files..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-touch "test.txt"
 rm -rf *
 
 # Run assetfinder
@@ -31,25 +30,19 @@ assetfinder -subs-only $domain | grep $domain > assetfinder.txt
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Getting subdomains from crt.sh..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-curl -s 'https://crt.sh/?q=%.'$domain'&output=json' | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > crt.txt
+curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u > crt.txt
 
 # Run Subfinder recursively
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running Subfinder recursively..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-subfinder -d $domain -silent -all -recursive -t 200 -silent -o subfinder-rescursive.txt
+subfinder -d $domain -all -recursive -t 200 -silent -o subfinder-recursive.txt
 
 # Run Findomain
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Running Findomain..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-findomain --quiet -t $domain > findomain.txt
-
-# Run Spyhunt
-echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running Spyhunt..."
-echo -e "${BLUE}-----------------------------------------${NC}"
-python3 ~/tools/spyhunt/spyhunt.py -s $domain -sv spyhunt.txt
+findomain -t $domain -q > findomain.txt
 
 # Merge all subdomains into all.txt
 echo -e "${BLUE}-----------------------------------------"
@@ -59,10 +52,10 @@ cat *.txt | sort -u > ../all.txt
 
 # Running HTTPX
 echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running httpx on all.txt..."
+echo -e "${GREEN}Running HTTPX on all.txt..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 cd ..
-cat all.txt | httpx -sc -title -cl -probe | grep -v "FAILED" | anew juicy-live.txt
+cat all.txt | httpx -t 100 -sc -title -cl -probe | grep -v "FAILED" | anew juicy-live.txt
 cat juicy-live.txt | sed 's|^[^/]*//||' | cut -d '/' -f 1 | cut -d " " -f 1 > new.txt 
 
 # Extracting important subdomains
@@ -76,15 +69,14 @@ clear
 echo -e "${BLUE}-----------------------------------------"
 echo -e "${GREEN}Getting endpoints..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-cat new.txt | gau > endpoints.txt
-waymore -i $domain -mode U -oU waymore.txt && cat waymore.txt | anew endpoints.txt
+gau $domain > endpoints.txt
 clear
 
-# Checking for xss
+# Checking for XSS
 echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Checking for xss..."
+echo -e "${GREEN}Checking for XSS..."
 echo -e "${BLUE}-----------------------------------------${NC}"
-cat endpoints.txt | uro | kxss | grep "<" | tee possible-xss.txt | notify
+cat endpoints.txt | gf xss | uro | kxss | grep "<" | tee possible-xss.txt | notify
 
 # Port Scan
 echo -e "${BLUE}-----------------------------------------"
@@ -92,15 +84,15 @@ echo -e "${GREEN}Running Port Scan..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 naabu -l new.txt -o ports.txt
 
-# Nucleifuzzer
+# Nuclei Fuzzer
 echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running NucleiFuzzer..."
+echo -e "${GREEN}Running Nuclei Fuzzer..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 nf -d $domain
 
 # Running Nuclei
 echo -e "${BLUE}-----------------------------------------"
-echo -e "${GREEN}Running nuclei scan..."
+echo -e "${GREEN}Running Nuclei scan..."
 echo -e "${BLUE}-----------------------------------------${NC}"
 nuclei -l new.txt -es info,low -rl 25 | tee vulns.txt | notify
 
